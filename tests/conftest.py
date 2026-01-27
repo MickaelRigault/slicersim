000f301d56dc8d473f4662bc7f98d54337cc5260
Simon Conseil <s.conseil@ip2i.in2p3.fr>
2026-01-27 11:51:55 +0100

    add config for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,17 @@
 import pytest
 
 from slicersim import iotools
+from slicersim.target import VirtualTarget
+
+
+def pytest_configure(config):
+    VirtualTarget._DEFAULT_CONFIG = {"instrument": "lazuli_test.toml"}
+
+
+def pytest_unconfigure(config):
+    VirtualTarget._DEFAULT_CONFIG = {"instrument": "lazuli.toml"}
 
 
 @pytest.fixture
 def default_config():
-    return iotools.get_config(instrument="lazuli.toml")
+    return iotools.get_config(instrument="lazuli_test.toml")
