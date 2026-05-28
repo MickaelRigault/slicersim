cb35dfd903836a578da54c17a6adfc7b9529f231
MickaelRigault <mick.rigault@gmail.com>
2026-05-28 11:11:25 +0200

    version up

--- src/slicersim/__init__.py
+++ src/slicersim/__init__.py
@@ -8,7 +8,7 @@ Top-level functionalities of this package are currently built for the Lazuli Spa
 Telescope, but the code is generic and can be extended to any Integral Field
 Unit (slicer or micro-lens array).
 """
-__version__ = '0.33.1'
+__version__ = '0.34.0'
 
 # Top level simulations
 from .simulation import * # noqa: F403
